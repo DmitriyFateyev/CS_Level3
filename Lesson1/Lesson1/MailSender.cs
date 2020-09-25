3f4671a88529a4e2b46c3b0999258397e10130f7
Dmitriy Fateyev <dmitriy.fateyev86@gmail.com>
2020-09-25 20:30:49 +0400

    Initial commit

---
+++ Lesson1/Lesson1/MailSender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class MailSender
+    {
+        static string login;
+        static string password;
+        
+            
+
+            MailMessage mm = new MailMessage("ds0090@yandex.ru", "dmitriy.fateyev86@gmail.com");
+            mm.Subject = "Заголовок письма";
+            mm.Body = "Содержимое письма";
+            mm.IsBodyHtml = false;
+
+            SmtpClient sc = new SmtpClient("smtp.yandex.ru", 567);
+            sc.EnableSsl = true;
+            sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+            sc.UseDefaultCredentials = false;
+            sc.Credentials = new NetworkCredential(login, password);
+            try
+            {
+                sc.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Cannot send e-mail! Error: {ex.Message}");
+                Console.ReadLine();
+            }
+        }
+    
+
---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static string login;
+        static string password;
+
+        static void Main(string[] args)
+        {
+            #region CREDENTIALS
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(@"..\..\..\credentials\users.txt", Encoding.Default))
+                {
+                    var line = sr.ReadLine().Split(':');
+                    login = line[0];
+                    password = line[1];
+                }
+            }
+            catch
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read");
+            }
+            #endregion
+        }
+    }
+}
